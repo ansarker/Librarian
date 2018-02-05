3838d418415e0fcf4a179196610896b24bee6fd2
cis101 <cis101@secs.iub.edu.bd>
2018-02-05 09:34:08 +0600

    Class work not done yet

--- librarian.js
+++ librarian.js
@@ -3,13 +3,18 @@ var library = [
   { title: "The Old Man and the Sea", year: "1952", author: "Ernest Hemingway", publisher: "Charles Scribner's Sons" },
   { title: "Mythos: A Retelling of the Myths of Ancient Greece", year: "1981", author: "Stephen Fry", publisher: "Penguin" },
   { title: "The Mythical Man-Month", year: "1975", author: "Fred Brooks", publisher: "Addison-Wesley" },
-  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" }
+  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" },
   /* Task 2 */
+  { title: "Following the Equator", year: "1897", author: "Mark Twain", publisher: "Sun-Times Media Group"}
 ];
 
 /* Task 1 */
 function getBookTitle( index ){
-  //add code
+  if(index >= 0 && index < library.length) {
+    return library[index].title;
+  } else {
+    return console.log("Not in list");
+  }
 }
 
 console.log(getBookTitle(3));
@@ -17,6 +22,8 @@ console.log(getBookTitle(3));
 /* Task 3 */
 function addBook( book ){
   //add code
+  library.push(book);
+  return library;
 }
 
 var newBook = {};
@@ -25,6 +32,19 @@ console.log(addBook(newBook));
 /* Task 4 */
 function bookByAuthor(){
   //add code
+  var sortable = [];
+  for(var key in library)
+    if(library.hasOwnProperty(key))
+      sortable.push([key, library[key]]); // each item is an array in format [key, value]
+  
+  // sort items by value
+  sortable.sort(function(a, b)
+  {
+    var x=a[1].toLowerCase(),
+      y=b[1].toLowerCase();
+    return x<y ? -1 : x>y ? 1 : 0;
+  });
+  return sortable;
 }
 
 console.log(bookByAuthor());
